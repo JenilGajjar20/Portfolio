a209b09ec4bf49c5516b79a2b2c7dfc9614b77ff
Jenil Gajjar <68738624+JenilGajjar20@users.noreply.github.com>
2023-05-14 02:01:00 +0530

    merge change

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,10 @@ import Navbar from "@/components/navbar/Navbar.vue";
   </header>
 
   <router-view />
+
+  <!-- <footer class="footer">
+    <Footer/>
+  </footer> -->
 </template>
 
 <style scoped></style>
