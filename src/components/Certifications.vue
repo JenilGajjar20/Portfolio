efd48108f5a52cf93fb892f9392a6a8e09ff36f0
Jenil Gajjar <jenil20gajjar@gmail.com>
2024-07-26 12:31:03 +0530

    certificates section completed

--- src/components/Certifications.vue
+++ src/components/Certifications.vue
@@ -7,11 +7,30 @@
         </div>
         <div class="certificates-data">
           <div
-            class=""
+            class="certificate"
             v-for="certificate in certificates"
             :key="certificate.id"
           >
-            {{ certificate }}
+            <div class="flex md:items-center justify-between">
+              <h4 class="font-bold text-xl md:text-2xl">
+                {{ certificate.title }},
+                <span class="font-normal text-base md:text-xl">
+                  {{ certificate.platform }}
+                </span>
+              </h4>
+              <p class="whitespace-nowrap text-sm text-gray-400 md:text-base">
+                {{ certificate.month + " " + certificate.year }}
+              </p>
+            </div>
+            <ul>
+              <li
+                v-for="desc in certificate.description"
+                :key="desc"
+                class="list-disc ml-4"
+              >
+                {{ desc }}
+              </li>
+            </ul>
           </div>
         </div>
       </div>
@@ -30,4 +49,13 @@ onMounted(() => {
 });
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+.certificates-data {
+  @apply pt-5 md:pt-10 space-y-5 md:space-y-8;
+  .certificate {
+    ul {
+      @apply mt-4;
+    }
+  }
+}
+</style>
