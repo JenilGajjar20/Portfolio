aa207c190896fd62a0be7b34bfeb10fa56788033
Jenil Gajjar <jenil20gajjar@gmail.com>
2024-08-01 20:51:34 +0530

    blogs section - WIP

--- src/components/blogs/Index.vue
+++ src/components/blogs/Index.vue
@@ -1,5 +1,11 @@
 <template>
-  <div>My blogs</div>
+  <div class="blogs">
+    <div class="blogs-content">
+      <h1 class="blogs-content__title">
+        🚀 Explore My Tech Journey: Blog Posts & Articles 📚
+      </h1>
+    </div>
+  </div>
 </template>
 
 <script setup></script>
--- src/views/BlogView.vue
+++ src/views/BlogView.vue
@@ -1,9 +1,35 @@
 <template>
   <main class="container">
     <Blogs />
+
+    <BlogsLatest />
   </main>
 </template>
 
 <script setup>
 import Blogs from "@/components/blogs/Index.vue";
+import BlogsLatest from "@/components/blogs/Latest.vue";
 </script>
+
+<style lang="scss">
+.blogs {
+  @apply py-8 md:py-12;
+  &-content {
+    @apply bg-charcoal rounded p-4 text-center md:p-8 lg:p-12;
+    &__title {
+      @apply text-bright_yellow text-2xl font-oswald md:text-6xl;
+    }
+  }
+  &-latest {
+    @apply lg:flex lg:items-start lg:gap-6;
+    &__img {
+      @apply mb-4 lg:mb-0;
+      img {
+        @apply w-full h-72 md:h-96 lg:h-full rounded-lg;
+      }
+    }
+    &__data {
+    }
+  }
+}
+</style>
