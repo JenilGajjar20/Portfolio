9a701eaa22ba9ff8ea7b115f6f134152b919fc61
Jenil Gajjar <jenil20gajjar@gmail.com>
2024-08-04 00:00:24 +0530

    blog page completeed

---
+++ src/components/blogs/Articles.vue
@@ -0,0 +1,57 @@
+<template>
+  <h4 class="mb-4 text-xl font-bold md:mb-8 md:text-5xl">All Articles</h4>
+  <div class="blogs-articles">
+    <div
+      v-for="article in articles"
+      :key="article.id"
+      class="bg-gray-800 rounded overflow-hidden lg:flex lg:items-center lg:gap-6"
+    >
+      <div class="blogs-articles__img">
+        <img
+          :src="`../src/assets/images/${article.image}`"
+          :alt="article.title"
+        />
+      </div>
+      <div class="blogs-articles__data">
+        <div
+          class="flex items-center gap-4 text-gray-600 text-sm md:text-base pb-4"
+        >
+          <p>{{ article.platform }}</p>
+          <span>|</span>
+          <p>{{ article.date_published }}</p>
+        </div>
+        <div>
+          <a
+            :href="article.link"
+            target="_blank"
+            class="text-2xl font-oswald md:text-4xl hover:text-bright_yellow"
+          >
+            {{ article.title }}
+          </a>
+          <p class="mt-4 text-gray-500 xl:mt-6 xl:text-lg">
+            {{ article.desc.substring(0, 250) + "..." }}
+          </p>
+        </div>
+        <div class="mt-4 flex items-center gap-2 text-gray-600">
+          <IconsMdiRead />
+          <p>{{ article.read_time }} min read</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import articlesData from "@/data/articles/data.json";
+import IconsMdiRead from "@/components/icons/MdiRead.vue";
+
+import { onMounted, ref } from "vue";
+
+const articles = ref([]);
+
+onMounted(() => {
+  articles.value = articlesData;
+});
+</script>
+
+<style lang="scss"></style>
--- src/views/BlogView.vue
+++ src/views/BlogView.vue
@@ -3,12 +3,18 @@
     <Blogs />
 
     <BlogsLatest />
+
+    <HLine />
+
+    <BlogsArticles />
   </main>
 </template>
 
 <script setup>
 import Blogs from "@/components/blogs/Index.vue";
 import BlogsLatest from "@/components/blogs/Latest.vue";
+import BlogsArticles from "@/components/blogs/Articles.vue";
+import HLine from "@/components/HLine.vue";
 </script>
 
 <style lang="scss">
@@ -28,7 +34,16 @@ import BlogsLatest from "@/components/blogs/Latest.vue";
         @apply w-full h-72 md:h-96 lg:h-full rounded-lg;
       }
     }
+  }
+  &-articles {
+    @apply grid grid-cols-1 gap-5;
+    &__img {
+      img {
+        @apply h-72 w-96 p-3 mx-auto md:w-full lg:pr-0 lg:w-96;
+      }
+    }
     &__data {
+      @apply w-full px-6 py-3 md:py-6 lg:pl-0;
     }
   }
 }
